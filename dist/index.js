da91ff3b5f07651940bb416355a9444db9fb4e9c
alfenfebral <alfenfebral@gmail.com>
2018-06-09 01:50:02 +0700

    Fix

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,49 @@
-var express = require('express');
-var pug = require('pug');
-var path = require('path');
-var app = express();
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function (req, res, next) {
-    // res.render('index');
-    res.send(greeting("Hello Alfen, Your age is ") + getAge(22));
-});
-function greeting(greet) {
-    return greet;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var debug = require("debug");
+var http = require("http");
+var server_1 = require("./server");
+debug('ts-express:server');
+var port = normalizePort(process.env.PORT || 3000);
+server_1.default.set('port', port);
+console.log("Server listening on port " + port);
+var server = http.createServer(server_1.default);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+function normalizePort(val) {
+    var port = (typeof val === 'string') ? parseInt(val, 10) : val;
+    if (isNaN(port)) {
+        return val;
+    }
+    else if (port >= 0) {
+        return port;
+    }
+    else {
+        return false;
+    }
 }
-function getAge(age) {
-    return age;
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    var bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + " requires elevated privileges");
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + " is already in use");
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 }
-app.listen(3000);
+function onListening() {
+    var addr = server.address();
+    var bind = (typeof addr === 'string') ? "pipe " + addr : "port " + addr.port;
+    debug("Listening on " + bind);
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,44 @@
-const express = require('express');
-const pug = require('pug');
-const path = require('path');
+import * as debug from 'debug';
+import * as http from 'http';
 
-let app = express();
+import Server from './server';
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+debug('ts-express:server');
 
-app.use(express.static(path.join(__dirname, 'public')));
+const port = normalizePort(process.env.PORT || 3000);
+Server.set('port', port);
 
-app.get('/', function (req, res, next) {
-  // res.render('index');
-  res.send(greeting("Hello Alfen, Your age is ") + getAge(22));
-})
+console.log(`Server listening on port ${port}`);
 
-function greeting(greet: string): string {
-  return greet;
+const server = http.createServer(Server);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val: number|string): number|string|boolean {
+  const port: number = (typeof val === 'string') ? parseInt(val, 10) : val;
+  if (isNaN(port)) { return val; } else if (port >= 0) { return port; } else { return false; }
 }
 
-function getAge(age: number): number {
-  return age;
+function onError(error: NodeJS.ErrnoException): void {
+  if (error.syscall !== 'listen') { throw error; }
+  const bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
-app.listen(3000);
+function onListening(): void {
+  const addr = server.address();
+  const bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+}
